1f66db14789c777a8b149e17ad8a55ef746c6e30
villagrat <elsainetedelautrec@gmail.com>
2023-07-16 19:10:35 -0300

    exercise 02: typing components

--- src/02-components/04-typing-components.problem.tsx
+++ src/02-components/04-typing-components.problem.tsx
@@ -1,6 +1,21 @@
-import React from "react";
+import React from 'react';
 
-export const Button = (props: unknown) => {
+// Sol 1
+interface Props {
+  className: string;
+}
+// export const Button = (props: Props) => {...}
+// Sol 2 - same as Sol 1 but type syntax
+// Sol 3 - inline definition ~ (props: { className: string }) => {...}
+/* Sol 4 - similar to Sol 3 but destructure props you'll use
+
+export const Button = ({ className }: { className: string }) => {
+  return <button className={className}></button>;
+}
+
+*/
+
+export const Button = (props: Props) => {
   return <button className={props.className}></button>;
 };
 
