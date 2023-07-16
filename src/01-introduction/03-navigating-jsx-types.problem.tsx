c460c97b725dc97a416a423197feb7af9820fa40
villagrat <elsainetedelautrec@gmail.com>
2023-07-16 18:50:41 -0300

    exercise 01: navigating type declaration files

--- src/01-introduction/03-navigating-jsx-types.problem.tsx
+++ src/01-introduction/03-navigating-jsx-types.problem.tsx
@@ -1,12 +1,24 @@
 export const Component = () => {
   return (
+    // Q: How do I know all the props that this component takes?
+    // A: cmd-click @ div --> go to declaration file --> React.DetailedHTMLProps
     <div
-      // How do I figure out what type aria-posinset expects?
-      aria-posinset={}
-      // How do I figure out what type onChange expects?
-      onChange={}
+      // Q: How do I figure out what type aria-posinset expects?
+      // A: hover over the prop to see the type
+      // simple eg
+      aria-posinset={1} // <-- hover to see type is number | undefined from React.AriaAttributes
+      // complex eg
+      // Q: How do I figure out what type onChange expects?
+      onChange={(e) => {}} // <-- hover to see type is React.FormEventHandler<HTMLDivElement> | undefined
+      // ^ cmd-click to see what it expects to receive
 
-      // How do I get autocomplete with JSX?
+      // Q: How do I get autocomplete with JSX?
+      // A: ctrl-space brings expected props on this OBJ
     />
   );
 };
+
+// Inspecting complex types (if you're reading this you are in the bad ending GOOD LUCK)
+// 1. Declare the type you want to inspect
+// 2. cmd + click @ FormEventHandler to see what it expects to receive
+type Example = React.FormEventHandler<HTMLDivElement> | undefined;
